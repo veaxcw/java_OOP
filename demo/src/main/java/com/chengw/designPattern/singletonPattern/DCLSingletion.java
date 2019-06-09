dd2798d0fb5de0acb333f8a795956b25594da807
veaxcw <878899580@qq.com>
2019-06-09 20:57:08 +0800

    end holiday

--- demo/src/main/java/com/chengw/designPattern/singletonPattern/DCLSingletion.java
+++ demo/src/main/java/com/chengw/designPattern/singletonPattern/DCLSingletion.java
@@ -4,7 +4,8 @@ package com.chengw.designPattern.singletonPattern;
 /**
  * 双重检查锁定,来减少开销
  * 使用volatile 保障了可见性和有序性
- * **/
+ *
+ * @author chengw**/
 public class DCLSingletion {
 
     private static volatile DCLSingletion instance;
---
+++ demo/src/main/java/com/chengw/thread/lock/cas/CASBasedCounter.java
@@ -0,0 +1,75 @@
+package com.chengw.thread.lock.cas;
+
+import com.chengw.thread.utils.Tools;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+
+/**
+ * @author chengw
+ */
+public class CASBasedCounter {
+
+    private volatile long count;
+    private AtomicLongFieldUpdater<CASBasedCounter> fieldUpdater;
+
+    public CASBasedCounter(){
+        fieldUpdater = AtomicLongFieldUpdater.newUpdater(CASBasedCounter.class,"count");
+    }
+
+    public long value(){
+
+        return count;
+    }
+
+
+    public void increment(){
+
+        long oldValue;
+        long newValue;
+
+        do{
+            oldValue = count;
+            newValue = oldValue + 1;
+        }while ( !compareAndSwap(oldValue,newValue));
+
+    }
+
+
+    private boolean compareAndSwap(long oldValue, long newValue){
+
+        return fieldUpdater.compareAndSet(this,oldValue,newValue);
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final CASBasedCounter counter  = new CASBasedCounter();
+        Thread t;
+        Set<Thread> threads = new HashSet<>();
+        /**新建20个写 线程**/
+        for(int i =0;i < 20;i++){
+            t = new Thread(() -> {
+                Tools.randomPause(50);
+                counter.increment();
+            });
+            threads.add(t);
+        }
+
+        for(int i =0;i < 8;i++){
+            t = new Thread(() -> {
+                Tools.randomPause(50);
+                System.out.println("当前值：" + counter.value());
+            });
+            threads.add(t);
+        }
+
+        Tools.startAndWaitTerminated(threads);
+        System.out.println("final count:" + counter.value());
+
+
+
+    }
+
+
+}
