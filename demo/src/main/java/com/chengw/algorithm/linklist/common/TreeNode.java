14889d45f942320b3b2f8ba4fc62085d46f6c917
veaxcw <xveacw8023>
2019-06-27 17:50:20 +0800

    update

---
+++ demo/src/main/java/com/chengw/algorithm/linklist/common/TreeNode.java
@@ -0,0 +1,15 @@
+package com.chengw.algorithm.linklist.common;
+
+/**
+ * @author chengw
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int data) {
+        this.val = data;
+    }
+}
