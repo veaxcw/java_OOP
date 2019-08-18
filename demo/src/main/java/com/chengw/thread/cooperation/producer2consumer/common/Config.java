a28f4d94f5039e9401493c92a007cf9abcb9072d
veaxcw <878899580@qq.com>
2019-08-18 11:07:55 +0800

    删除 class

---
+++ demo/src/main/java/com/chengw/thread/cooperation/producer2consumer/common/Config.java
@@ -0,0 +1,16 @@
+package com.chengw.thread.cooperation.producer2consumer.common;
+
+/**
+ * @author veax
+ */
+public  class Config {
+
+    public static String BASE_PATH = "/home/veax/Java/java/demo/src/main/java/com/chengw/thread/cooperation/producer2consumer";
+
+    public static final String IN_PATH =  "/data/in.dat";
+
+    public static final String LOG_FILE_PATH = "/data/log";
+
+
+    public static final String CONSUMER_THREAD_CLASS_PATH = "com.chengw.thread.cooperation.producer2consumer.consumer.MultithreadedStatTask";
+}
